be46216d9a537f72545b7e92a60c464fcf41c75e
Caleb Villalba <caleb@concept7.nl>
2023-12-11 16:27:02 -0500

    main layout first commit

--- client/src/containers/Dashboard/index.jsx
+++ client/src/containers/Dashboard/index.jsx
@@ -1,9 +1,11 @@
 import Dashboard from "../../components/Dashboard";
+import MainLayout from "../Layout";
+
 function DashboardContainer() {
   return (
-    <>
+    <MainLayout>
       <Dashboard />
-    </>
+    </MainLayout>
   );
 }
 export default DashboardContainer;
